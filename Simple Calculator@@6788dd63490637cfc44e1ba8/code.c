c6969174ab738e9515e343d523e97debb4279a8a
nanditadhiman <nanditadhiman11@gmail.com>
2025-03-15 18:43:49 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,41 +1,19 @@
-#include <stdio.h>
-
-int main() {
-    int a, b;
-    char c;  // Declare the operator before reading it
-
-    // Step 1: Take first number as input
-    printf("Enter first number: ");
-    scanf("%d", &a);
-
-    // Step 2: Take operator input (with space before %c to remove newline issue)
-    printf("Enter an operator (+, -, *, /): ");
-    scanf(" %c", &c);  // FIX: Space before %c to remove input buffer issue
-
-    // Step 3: Take second number as input
-    printf("Enter second number: ");
-    scanf("%d", &b);
-
-    // Step 4: Perform calculation based on operator
-    if (c == '+') {
-        printf("%d", a + b);
-    } 
-    else if (c == '-') {
-        printf("%d", a - b);
-    } 
-    else if (c == '*') {
-        printf("%d", a * b);
-    } 
-    else if (c == '/') {
-        if (b != 0) {  // Prevent division by zero
-            printf("%d", a / b);
-        } else {
-            printf("Error");
-        }
-    } 
-    else {
-        printf("Error");
+#include<stdio.h>
+int main(){
+    int a,b;
+    char c;
+    scanf("%d %d %c",&a,&b,&c);
+    if(c=='+'){
+        printf("%d", a+b);
     }
-
+    else if(c=='-'){
+        printf("%d",a-b);
+    }
+    else if(c=='*'){
+        printf("%d",a*b);
+    }
+    else if(c=='/'){
+        printf("%d",a/b);
+    }
     return 0;
 }
