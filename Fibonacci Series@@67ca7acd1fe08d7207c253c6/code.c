362e3f86e4922d144bb300aff00b30e247e3a0df
nanditadhiman <nanditadhiman11@gmail.com>
2025-03-29 20:28:16 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,26 +1,22 @@
-#include <stdio.h>
-
-// Function to compute Fibonacci using recursion
-int fibonacciSeries(int n);
-
-int main() {
-    int n;
-    printf("Enter the number of terms: ");
-    scanf("%d", &n);
-
-    // Print Fibonacci series
-    for (int i = 0; i < n; i++) {
-        printf("%d\t", fibonacciSeries(i));
+#include<stdio.h>
+int fibonacciSeries(int N);
+int main(){
+    int N;
+    scanf("%d",&N);
+    if(n<=0){
+        printf("Invalid input\n");
     }
-    printf("\n"); // Formatting for clean output
-
+    for(int i=0;i<n;i++){
+        printf("%d\t",fibonacciSeries(i));
+    }
     return 0;
 }
-
-// Recursive Fibonacci function
-int fibonacciSeries(int n) {
-    if (n == 0) return 0;
-    if (n == 1) return 1;
-    
-    return fibonacciSeries(n - 1) + fibonacciSeries(n - 2);
+int fibonacciSeries(int N){
+    if(n==1){
+        return 1;
+    }
+    else if(n==0){
+        return 0;
+    }
+    return fibonacciSeries(n-1) + fibonacciSeries(n-2);
 }
