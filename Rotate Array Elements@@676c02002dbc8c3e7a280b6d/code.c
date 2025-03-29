15c545667480bc06fc03bd767c753e156d215870
nanditadhiman <nanditadhiman11@gmail.com>
2025-03-29 23:01:22 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,22 +1,46 @@
-#include<stdio.h>
-void reverse(int arr[],int n);
-void printarr(int arr[],int n);
-int main(){
+#include <stdio.h>
+
+// Function Declarations
+void reverse(int arr[], int n);
+void printarr(int arr[], int n);
+
+int main() {
     int n;
-    scanf("%d",&n);
-    reverse(arr,n);
-    printarr(arr,n);
+    
+    // Take array size as input
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    
+    int arr[n];  // Declare array dynamically based on input
+    
+    // Take array elements as input
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Reverse the array
+    reverse(arr, n);
+
+    // Print the reversed array
+    printf("Reversed array:\n");
+    printarr(arr, n);
+
     return 0;
 }
-void printarr(int arr[],int n){
-    for(int i=0;i<n;i++){
-        printf("%d\n",arr[i]);
+
+// Function to Print Array
+void printarr(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", arr[i]);
     }
 }
-void reverse(int arr[],int n){
-    if(int i=0;i<n/2;i++){
-        int temp=arr[i];
-        arr[i]=arr[n-i-1];
-        arr[n-i-1]=temp;
+
+// Function to Reverse Array
+void reverse(int arr[], int n) {
+    for (int i = 0; i < n / 2; i++) {  // Fixed syntax error
+        int temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
     }
 }
